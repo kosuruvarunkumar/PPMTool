87904ee95f1b444217452b22c229378391baf491
Kosuru Varun Kumar <kosuruvarunkumar@gmail.com>
2020-09-04 19:44:50 +0530

    Project Object, project controllers, project services creation

---
+++ ppmtool/src/main/java/com/example/ppmtool/controllers/ProjectController.java
@@ -0,0 +1,29 @@
+/**
+ * Created by varun on 31/08/20
+ */
+
+package com.example.ppmtool.controllers;
+
+import com.example.ppmtool.domain.Project;
+import com.example.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+
+        Project proj =  projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+}
---
+++ ppmtool/src/main/java/com/example/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+/**
+ * Created by varun on 31/08/20
+ */
+
+package com.example.ppmtool.repositories;
+
+import com.example.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
