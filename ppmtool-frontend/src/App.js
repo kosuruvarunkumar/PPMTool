025e88a14f177fb81baa47a20b1c7ced0136824b
Kosuru Varun Kumar <kosuruvarunkumar@gmail.com>
2020-09-10 21:19:08 +0530

    creation of components and basic styling

--- ppmtool-frontend/src/App.js
+++ ppmtool-frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./Components/Dashboard";
+import Header from "./Components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Dashboard />
+        </div>
+    );
 }
 
 export default App;
