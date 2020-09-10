e358323cf0cc039b7bececafd66067cef9059f18
Kosuru Varun Kumar <kosuruvarunkumar@gmail.com>
2020-09-10 21:39:34 +0530

    Setting up routers

--- ppmtool-frontend/src/App.js
+++ ppmtool-frontend/src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import Dashboard from "./Components/Dashboard";
 import Header from "./Components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./Components/Project/AddProject";
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Dashboard />
-        </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/addProject" component={AddProject} />
+            </div>
+        </Router>
     );
 }
 
